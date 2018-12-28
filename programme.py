8fbdb77b92ac01c72c65bf0812f949f692ac85b8
HugoFrs <46021391+HugoFrs@users.noreply.github.com>
2018-12-28 22:22:26 +0100

    Add files via upload

--- programme.py
+++ programme.py
@@ -1,1 +1,35 @@
-print("test")
+joueur={}
+nbr_jr=0
+
+def nouveau_joueur():
+    global nbr_jr   
+    nom_du_joueur=input('nom du joueur:')
+    joueur[nbr_jr]={"nom": nom_du_joueur , "jetons": 500}
+    nbr_jr=nbr_jr+1
+
+def perdu():
+    for i in range(nbr_jr):
+        if joueur[i].get("jetons")==0:
+            del joueur[i]
+
+def paquet_actuel():
+    global paquet, nouvelle_carte
+
+    x=randrange(0,len(paquet))
+    nouvelle_carte=paquet[x]
+    paquet.remove(paquet[x])
+
+    return nouvelle_carte
+
+
+def paquet_de_carte():
+    global paquet
+    paquet=['as_de_carreau','as_de_coeur','as_de_pic','as_de_trefle','2_de_carreau','2_de_coeur','2_de_pic','2_de_trefle',
+            '3_de_carreau','3_de_coeur','3_de_pic','3_de_trefle','4_de_carreau','4_de_coeur','4_de_pic','4_de_trefle',
+            '5_de_carreau','5_de_coeur','5_de_pic','5_de_trefle','6_de_carreau','6_de_coeur','6_de_pic','6_de_trefle',
+            '7_de_carreau','7_de_coeur','7_de_pic','7_de_trefle','8_de_carreau','8_de_coeur','8_de_pic','8_de_trefle',
+            '9_de_carreau','9_de_coeur','9_de_pic','9_de_trefle','10_de_carreau','10_de_coeur','10_de_pic','10_de_trefle',
+            'valet_de_carreau','valet_de_coeur','valet_de_pic','valet_de_trefle',
+            'reine_de_carreau','reine_de_coeur','reine_de_pic','reine_de_trefle','roi_de_carreau','roi_de_coeur','roi_de_pic','roi_de_trefle']
+    
+    
